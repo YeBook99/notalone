de56750d9549a3ce932c83c6da85b2a3dd3ab1a1
YeBook99 <pop0131@naver.com>
2024-12-06 10:57:53 +0900

    create notalone-api

---
+++ notalone-api/src/main/java/com/ss/notaloneapi/NotaloneApiApplication.java
@@ -0,0 +1,13 @@
+package com.ss.notaloneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotaloneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotaloneApiApplication.class, args);
+	}
+
+}
